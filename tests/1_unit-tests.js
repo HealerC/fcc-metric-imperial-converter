d5be79fb4a716523ce07e27d0d9fd6a10d76c55d
Uyi <ihazauyi@gmail.com>
2022-04-26 22:50:04 +0100

    refactor: Make every assertion on its own test as required by FCC

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -6,23 +6,37 @@ let convertHandler = new ConvertHandler();
 
 suite('Unit Tests', function(){
   suite('Input Numbers', function() {
-    test('#readNumberInput', function() {
+    test('#extractWholeNumber', function() {
       assert.equal(convertHandler.getNum("27km"), 27, 
                     'Should extract whole number from input');
+    });
+    test('#extractNumberWithDecimal', function() {
       assert.equal(convertHandler.getNum("27.5mi"), 27.5, 
                     'Should be able to extract numbers with decimal point');
+    });
+    test('#extractNumberWithOnlyUnit', function() {
+      assert.equal(convertHandler.getNum("lbs"), 1, 
+                    'Should default to 1 if only unit is specified');
+    });
+    test('#extractFraction', function() {
       assert.equal(convertHandler.getNum("27/3gal"), 9, 
                     'Should be able to read fractional input');
+    });
+    test('#extractFractionWithDecimal', function() {
       assert.equal(convertHandler.getNum("27.5/0.5L"), 55, 
                     'Should be able to read fractional input with decimal point');
+    });
+    test('#errorOnDoubleFraction', function() {
       assert.isNotOk(convertHandler.getNum("27/5/1999km"), 
-                    'Double fractions should not work');    
+                    'Double fractions should not work');
     });
   });
   suite('Input Units', function(){
-    test('#readUnitInput', function() {
+    test('#extractUnit', function(){
       assert.equal(convertHandler.getUnit("27km"), "km", 
                     'Should extract unit from input');
+    });
+    test('#errorOnInvalidUnit', function(){
       assert.isNull(convertHandler.getUnit("27watts"), 
                     'Non-valid input unit');
     });
@@ -34,14 +48,25 @@ suite('Unit Tests', function(){
       assert.equal(convertHandler.spellOutUnit("l"), "litre", 
                     'Correctly spell out valid units');
     });
+    
   });
   suite('Converter', function(){
-    test('#converter', function(){
+    test('#galToL', function(){
       assert.deepEqual(convertHandler.convert('1', 'gal'), {num: 3.78541, unit: 'L'});
+    });
+    test('#LToGal', function(){
       assert.deepEqual(convertHandler.convert('1', 'l'), {num: 0.26417, unit: 'gal'});
+    });
+    test('#miToKm', function(){
       assert.deepEqual(convertHandler.convert('1', 'mi'), {num: 1.60934, unit: 'km'});
+    });
+    test('#kmToMi', function(){
       assert.deepEqual(convertHandler.convert('1', 'km'), {num: 0.62137, unit: 'mi'});
+    });
+    test('#kgToLbs', function(){
       assert.deepEqual(convertHandler.convert('1', 'kg'), {num: 2.20462, unit: 'lbs'});
+    });
+    test('#LbsToKg', function(){
       assert.deepEqual(convertHandler.convert('1', 'lbs'), {num: 0.45359, unit: 'kg'});
     });
   });
